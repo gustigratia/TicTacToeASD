302e990d1196eea4c38d9773d04ad83aa98965d3
gustigratia <gustigratia706@gmail.com>
2024-12-20 17:49:39 +0700

    add header

--- TicTacToe/src/Othello/Cell.java
+++ TicTacToe/src/Othello/Cell.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231083 - Arya Wiraguna Dwiputra
+ * 2 - 5026231097 - Gusti Gratia Delpiera
+ * 3 - 5026231152 - M. Naufal Erwin Effendi
+ */
 package Othello;
 
 import java.awt.*;
